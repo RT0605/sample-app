ba25d4459fc501567b54fb550bcd78ce4ee016f4
RT0605 <funky.heart619@gmail.com>
2025-07-10 15:38:41 +0900

    adjust_string関数の修正内容をコミット

--- initialize.py
+++ initialize.py
@@ -135,8 +135,7 @@ def initialize_retriever():
     db = Chroma.from_documents(splitted_docs, embedding=embeddings)
 
     # ベクターストアを検索するRetrieverの作成
-    st.session_state.retriever = db.as_retriever(search_kwargs={"k": 3})
-
+    st.session_state.retriever = db.as_retriever(search_kwargs={"k": 5})   # ←★★★ ここを3→5に修正！
 
 def initialize_session_state():
     """
